a405e75a503423ba42c19eb961fe39eeb2694a07
bawuj <bawujdevchain@gmail.com>
2024-11-19 09:01:53 +0100

    spinner

--- src/components/spinner/LoadingSpinner.jsx
+++ src/components/spinner/LoadingSpinner.jsx
@@ -4,10 +4,16 @@ import './LoadingSpinner.css'
 const LoadingSpinner = () => {
   return (
     <div className='loading-overlay'>
+        <div className="text">
+         <p className="loading-text">
+          {'ICAST SCHOOLS.......'.split('').map((letter, index) => (
+            <span key={index} className={`letter letter-${index % 5}`}>
+              {letter}
+            </span>
+          ))}
+        </p>
+      </div>
         <div className="spinner">
-        </div>
-        <div className='text'>
-        <p className="loading-text">Loading.......</p>
         </div>
     </div>
   )
